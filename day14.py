ba4ee8995869a1ddcc66c24077e8ab8f9a3eb05b
会先飞的鸟并不笨 <38178228+cestlavieqiang@users.noreply.github.com>
2019-07-09 09:02:25 +0800

    Update day14.py

--- day14.py
+++ day14.py
@@ -36,3 +36,17 @@ class Solution():
             else:
                 digits[-(1+i)] += 1
                 return digits
+            
+            
+            
+# 解法二：优化
+class Solution():
+    def plusOne(self, digits):
+        for i in range(len(digits)-1, -1, -1):
+            if digits[i] != 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+        digits[0] = 1
+        digits.append(0)
+        return digits
