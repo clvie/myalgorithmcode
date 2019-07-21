7f26c6050126850f31d7d1ec27d7908dccd72d1a
会先飞的鸟并不笨 <38178228+cestlavieqiang@users.noreply.github.com>
2019-07-21 09:41:31 +0800

    Create day24.py

---
+++ day24.py
@@ -0,0 +1,40 @@
+"""
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/convert-sorted-array-to-binary-search-tree
+
+将一个按照升序排列的有序数组，转换为一棵高度平衡二叉搜索树。
+
+本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+
+示例:
+
+给定有序数组: [-10,-3,0,5,9],
+
+一个可能的答案是：[0,-3,9,-10,null,5]，它可以表示下面这个高度平衡二叉搜索树：
+
+      0
+     / \
+   -3   9
+   /   /
+ -10  5
+
+
+"""
+# Definition for a binary tree node.
+class TreeNode():
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution():
+    def sortedArrayToBST(self, nums):
+        if not nums:
+            return None
+        n = len(nums)
+        mid = n // 2
+        root = TreeNode(nums[mid])
+        root.left = self.sortedArrayToBST(nums[:mid])
+        root.right = self.sortedArrayToBST(nums[mid+1:])
+        return root
