424282d4503436cc141e27d680b3bc66d055ff8f
会先飞的鸟并不笨 <38178228+cestlavieqiang@users.noreply.github.com>
2019-08-28 08:38:40 +0800

    Update day47.py

--- day47.py
+++ day47.py
@@ -36,3 +36,21 @@ class Solution(object):
 
             set1.add(i)
         return False
+
+    
+    # 先使用sort函数进行排序，然后再寻找重复数字，同样使用了内置函数
+    class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        
+        
+        if not nums:
+            return False
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i-1] == nums[i]:
+                return True
+        return False
