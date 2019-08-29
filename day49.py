e37a0e52211e9cc81c51a39b6c5c61f3c007c33d
会先飞的鸟并不笨 <38178228+cestlavieqiang@users.noreply.github.com>
2019-08-29 09:03:07 +0800

    Create day49.py

---
+++ day49.py
@@ -0,0 +1,39 @@
+"""
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/power-of-two
+
+给定一个整数，编写一个函数来判断它是否是 2 的幂次方。
+
+示例 1:
+
+输入: 1
+输出: true
+解释: 20 = 1
+示例 2:
+
+输入: 16
+输出: true
+解释: 24 = 16
+示例 3:
+
+输入: 218
+输出: false
+
+
+"""
+
+
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n <= 0:
+            return False
+        while n > 1:
+            if n % 2 == 0:
+                n //= 2
+            else:
+                return False
+        return True
