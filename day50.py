e1d65f92f50f67c67fcf4c1bb1424f8b3ba5c72f
会先飞的鸟并不笨 <38178228+cestlavieqiang@users.noreply.github.com>
2019-08-30 09:11:32 +0800

    Update day50.py

--- day50.py
+++ day50.py
@@ -62,3 +62,32 @@ class Solution(object):
                 if dict1[n] != dict2.get(n):
                     return False
         return True
+    
+    
+    
+    
+    
+# efaultdict接受一个工厂函数作为参数，如下来构造：
+# dict =defaultdict( factory_function)
+#
+# 这个factory_function可以是list、set、str等等，作用是当key不存在时，
+# 返回的是工厂函数的默认值，比如list对应[ ]，str对应的是空字符串，set对应set( )，int对应0
+# 例如：
+from collections import defaultdict
+
+dict1 = defaultdict(int)
+dict2 = defaultdict(set)
+dict3 = defaultdict(str)
+dict4 = defaultdict(list)
+dict1[2] ='two'
+
+print(dict1[1])
+print(dict2[1])
+print(dict3[1])
+print(dict4[1])
+
+#结果：
+0
+set()
+
+[]
